73a3c1095adf9b13dbb4073211afd1847b847555
JaviM-AWS <javier.millan@devoteam.com>
2024-04-10 14:28:55 +0200

    tic tac toe casi

--- React/tic-tac-toe/src/App.jsx
+++ React/tic-tac-toe/src/App.jsx
@@ -19,17 +19,53 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
 
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+  
   const updateBoard = (index) => {
+    if (board[index] || winner) return
+   
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
   
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      
+      setWinner(newWinner)
+    }
   }
 
   return (
@@ -51,11 +87,37 @@ function App() {
           {TURNS.X}
         </Square>
 
-        <button>Reiniciar</button>
+        <button onClick={resetGame}>Reiniciar</button>
 
         <Square isSelected={turn === TURNS.O}>
           {TURNS.O}
         </Square>
+      </section>
+
+      <section>
+        {
+          winner != null && (
+            <section className='winner'>
+              <div className='text'>
+              <h2>
+                {
+                  winner === false ? 'Empate' : 'El ganador es: '
+                }
+                </h2>
+
+                <header className='win'>
+                  <Square>
+                    {winner}
+                  </Square>
+                </header>
+
+                <footer>
+                  <button onClick={resetGame}>Empezar de nuevo</button>
+                </footer>
+              </div>
+            </section>
+          )
+        }
       </section>
 
     </main> 
